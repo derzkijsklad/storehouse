cea0da9b0fec5ab77c6e68f0af392e6ff58a6185
VolodymyrMikhno <volodymyr3583@gmail.com>
2025-01-09 22:56:35 +0200

    added dotenv

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 
 
-
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT|| 5000;
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
