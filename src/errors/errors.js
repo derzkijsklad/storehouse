80881fcd9b92ef0a5b4a387f2ec523137d331e23
VolodymyrMikhno <volodymyr3583@gmail.com>
2025-01-10 07:20:16 +0200

    added cors

--- src/errors/errors.js
+++ src/errors/errors.js
@@ -4,6 +4,7 @@ export function getError(code, text) {
 export function errorHandler(error, req, res, next) {
     const status = error.code ?? 500;
     const text = error.text ?? "Unknown server error " + error;
-    res.status(status).end(text);
+    res.status(status)
+       .json({ text });
      
  }
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { errorHandler } from "./errors/errors.js";
 import { accounts_route } from "./routes/accounts.js";
 import { container_routes } from "./routes/containerRoutes.js";
+import cors from 'cors';
 
 
 dotenv.config();
@@ -11,6 +12,11 @@ const port = process.env.PORT|| 5000;
 const server = app.listen(port);
 server.on("listening", () => console.log(`listening on port ${server.address().port}`));
 app.use(express.json());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true
+}));
 app.use('/api/auth', accounts_route);
 app.use('/api', container_routes);
 
