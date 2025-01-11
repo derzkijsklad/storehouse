bbb7c63046ea8d1d95717facdfd6ddf9de104749
VolodymyrMikhno <volodymyr3583@gmail.com>
2025-01-11 20:17:51 +0200

    added localhost to cors

--- src/server.js
+++ src/server.js
@@ -12,7 +12,12 @@ const port = process.env.PORT|| 5000;
 const server = app.listen(port);
 server.on("listening", () => console.log(`listening on port ${server.address().port}`));
 app.use(express.json());
-
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    credentials: true 
+}));
+app.options('*', cors());
 app.use('/api/auth', accounts_route);
 app.use('/api', container_routes);
 
