571e678242382ce07436fbccec507230c2edaf52
VolodymyrMikhno <volodymyr3583@gmail.com>
2025-01-12 15:56:05 +0200

    removed unused imports

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import { errorHandler } from "./errors/errors.js";
 import { accounts_route } from "./routes/accounts.js";
 import { container_routes } from "./routes/containerRoutes.js";
-import cors from 'cors';
 import corsMiddleware from "./middlewares/corsMiddleware.js";
 
 
@@ -14,7 +13,7 @@ const server = app.listen(port);
 server.on("listening", () => console.log(`listening on port ${server.address().port}`));
 app.use(express.json());
 app.use(corsMiddleware);
-app.options('*', corsMiddleware);\
+app.options('*', corsMiddleware);
 
 app.use('/api/auth', accounts_route);
 app.use('/api', container_routes);
