d07ed321ad631dd15ef2c906686b00611a9c3b2a
VolodymyrMikhno <volodymyr3583@gmail.com>
2025-01-13 15:22:52 +0200

    added POSTGRES_URI to config

--- src/config/service.js
+++ src/config/service.js
@@ -4,6 +4,6 @@ import ContainerDataService from "../services/ContainerDataService.js";
 dotenv.config()
 import config from 'config'
 
-export const accountsService = new AccountsService(process.env.MONGO_URI, config.get('ACCOUNT_DB'));
+export const accountsService = new AccountsService(process.env.MONGO_URI , config.get('ACCOUNT_DB'));
 
-export const containersService = new ContainerDataService(process.env.POSTGRES_URI);
+export const containersService = new ContainerDataService(process.env.POSTGRES_URI || config.get('POSTGRES_URI'));
