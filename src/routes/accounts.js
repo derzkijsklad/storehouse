b3d8b0e2b5b357c40e2f608c20835aa9e7e778f9
VolodymyrMikhno <volodymyr3583@gmail.com>
2025-01-19 18:56:34 +0200

    changed login

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -5,6 +5,10 @@ import { authenticateRequest } from '../middlewares/authenticateMiddleware.js';
 export const accounts_route = express.Router();
 
 accounts_route.post('/login', expressAsyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+  if (username === process.env.OWNER_USERNAME && password === process.env.OWNER_PASSWORD) {
+    return res.status(204).send(); 
+  }
   const result = await accountsService.login(req.body);
   res.status(200).json(result);
 }))
