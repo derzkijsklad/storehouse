ae84dc5287a2e3f8e0803bb48dc0e459650b813d
VolodymyrMikhno <volodymyr3583@gmail.com>
2025-01-24 00:43:47 +0200

    change login for owner

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -4,11 +4,13 @@ import expressAsyncHandler from 'express-async-handler';
 import { authenticateRequest } from '../middlewares/authenticateMiddleware.js';
 export const accounts_route = express.Router();
 
+
 accounts_route.post('/login', expressAsyncHandler(async (req, res) => {
   const { username, password } = req.body;
-  if (username === process.env.OWNER_USERNAME && password === process.env.OWNER_PASSWORD) {
+  if (compare(username, process.env.OWNER_USERNAME) && compare(password, process.env.OWNER_PASSWORD)) {
     return res.status(204).send(); 
   }
+
   const result = await accountsService.login(req.body);
   res.status(200).json(result);
 }))
