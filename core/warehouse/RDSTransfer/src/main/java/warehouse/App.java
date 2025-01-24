dbff15021ee6f0e6763c7c5b08380cbd40e6020c
Nadelele <nadele@gmail.com>
2025-01-25 00:14:15 +0200

    optimized RDSTransfer lambda

--- core/warehouse/RDSTransfer/src/main/java/warehouse/App.java
+++ core/warehouse/RDSTransfer/src/main/java/warehouse/App.java
@@ -1,43 +1,54 @@
 package warehouse;
 
-import java.util.*;
-import java.util.logging.*;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent.DynamodbStreamRecord;
 
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-import software.amazon.awssdk.services.dynamodb.*;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest.Builder;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.sql.Statement;
-import java.sql.ResultSet;
-
 public class App {
-    
+
     private static final int TRESHOLD_PERCENT_VALUE = 50;
     private static final int MAX_VALUE = 20;
     private static final String DYNAMO_ORDERS_TABLE_NAME = "spot_lack_data";
     private static final String DEFAULT_LOGGING_LEVEL = "INFO";
     private static final String LOGGER_LEVEL = "LOGGER_LEVEL";
-    
+
     static DynamoDbClient clientDynamo = DynamoDbClient.builder().build();
     static Builder requestInsertLack;
     static Logger logger = Logger.getLogger("orders-transfer");
+    static Connection connection;
+    boolean tableIsCreated = false;
 
-    public void handleRequest(DynamodbEvent event, Context context) {
-        String dbEndpoint = System.getenv("RDS_HOST");
-        String dbUsername = System.getenv("RDS_USERNAME");
-        String dbPassword = System.getenv("RDS_PASSWORD");
-        String dbName = System.getenv("RDS_DB_NAME");
-
+    public void handleRequest(DynamodbEvent event, Context context) throws SQLException {
+        if (connection == null) {
+            try {
+                initConnection();
+            } catch (SQLException ex) {
+            }
+        }
+        if (tableIsCreated == false) {
+            logger.info("create table start");
+            createTable(connection);
+        }
         loggerSetUp();
         requestsSetUp();
         logger.info(event.toString());
@@ -56,7 +67,12 @@ public class App {
                     String timestamp = map.get("timestamp").getN();
                     logger.info(String.format("Received: record with spotId=%s, value=%f, timestamp=%s",
                             spotId, value, timestamp));
-                    putOrderInRDS(dbEndpoint, dbUsername, dbPassword, dbName, spotId, value, timestamp);    
+                    try {
+                        putOrderInRDS(connection, spotId, value, timestamp);
+                    } catch (SQLException e) {
+
+                        e.printStackTrace();
+                    }
                 } else {
                     logger.warning(r.getEventName() + " event name but should be INSERT");
                 }
@@ -65,41 +81,42 @@ public class App {
         }
     }
 
-    private void putOrderInRDS(String dbEndpoint, String dbUsername, String dbPassword, String dbName, String spotId, double value, String timestamp) {
+    private void initConnection() throws SQLException {
+        String dbEndpoint = System.getenv("RDS_HOST");
+        String dbUsername = System.getenv("RDS_USERNAME");
+        String dbPassword = System.getenv("RDS_PASSWORD");
+        String dbName = System.getenv("RDS_DB_NAME");
+        String tableName = System.getenv("RDS_TABLE_NAME");
         String RDSconnectionString = String.format("jdbc:postgresql://%s:5432/%s", dbEndpoint, dbName);
+        connection = DriverManager.getConnection(RDSconnectionString, dbUsername, dbPassword);
+        tableIsCreated = checkIfTableExists(connection, tableName);
+        logger.info(tableIsCreated + " - is table createdsam");
+    }
 
-        try (Connection connection = DriverManager.getConnection(RDSconnectionString, dbUsername, dbPassword)) {
+    private void putOrderInRDS(Connection connection, String spotId, double value, String timestamp)
+            throws SQLException {
 
-            // Check if the table exists
-			logger.info("checkiftableexists");
-            if (!checkIfTableExists(connection, "orders_data")) {
-                createTable(connection);
-            }
+        String sql = "INSERT INTO orders_data (spot_id, value, timestamp, is_closed) VALUES (?, ?, ?, false);";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        Timestamp postgresTimestamp = convertTimestamp(timestamp);
+        preparedStatement.setLong(1, Long.parseLong(spotId));
+        preparedStatement.setDouble(2, value);
+        preparedStatement.setTimestamp(3, postgresTimestamp);
+        logger.info(preparedStatement.toString());
+        preparedStatement.executeUpdate();
 
-            // Insert data into the table
-            String sql = "INSERT INTO orders_data (spot_id, value, timestamp, is_closed) VALUES (?, ?, ?, false);";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-            Timestamp postgresTimestamp = convertTimestamp(timestamp);
-            preparedStatement.setLong(1, Long.parseLong(spotId)); 
-            preparedStatement.setDouble(2, value); 
-            preparedStatement.setTimestamp(3, postgresTimestamp); 
-            logger.info(preparedStatement.toString());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     private boolean checkIfTableExists(Connection connection, String tableName) throws SQLException {
-        String checkTableSQL = "SELECT * FROM information_schema.tables WHERE table_name = '" + tableName + "';";
-        try (Statement stmt = connection.createStatement()) {
-            ResultSet rs = stmt.executeQuery(checkTableSQL);
-            return rs.next();  // If table exists, rs.next() will return true
-        }
+        DatabaseMetaData meta = connection.getMetaData();
+        logger.info("checking if table exists");
+        ResultSet resultSet = meta.getTables(null, null, tableName, new String[] { "TABLE" });
+        logger.info("checking if table exists - end");
+        return resultSet.next();
     }
 
     private void createTable(Connection connection) throws SQLException {
+        logger.info("create table method");
         String createTableSQL = "CREATE TABLE orders_data ("
                 + "id SERIAL PRIMARY KEY, "
                 + "spot_id BIGINT NOT NULL, "
@@ -117,8 +134,8 @@ public class App {
     private Timestamp convertTimestamp(String timestamp) {
         Timestamp postgresTimestamp = null;
         try {
-            long timestampLong = Long.parseLong(timestamp); // Convert string to long (Unix timestamp)
-            postgresTimestamp = new Timestamp(timestampLong); // Convert to PostgreSQL Timestamp
+            long timestampLong = Long.parseLong(timestamp);
+            postgresTimestamp = new Timestamp(timestampLong);
         } catch (NumberFormatException e) {
             logger.warning("Invalid timestamp format: " + timestamp);
         }
